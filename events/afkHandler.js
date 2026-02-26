a0632a10667823c13e014ac7c388ba6c1cf96cd1
huanisalright <tworejib@gmail.com>
2026-02-26 21:53:42 +0700

    Update: Improved AFK System (automatically delete message)

--- events/afkHandler.js
+++ events/afkHandler.js
@@ -1,39 +1,31 @@
-const { Events, EmbedBuilder } = require('discord.js');
-const { afkUsers } = require('../commands/utility/afk');
+const { EmbedBuilder } = require('discord.js');
+const { afkUsers } = require('../commands/utility/afk.js');
 
 module.exports = {
-    name: Events.MessageCreate,
+    name: 'messageCreate',
     async execute(message) {
-        if (message.author.bot || !message.guild) return;
+        if (message.author.bot) return;
 
         if (afkUsers.has(message.author.id)) {
-            const data = afkUsers.get(message.author.id);
             afkUsers.delete(message.author.id);
-
-            try {
-                await message.member.setNickname(data.nickname);
-            } catch (err) {
-            }
-
-            const welcomeBack = new EmbedBuilder()
-                .setDescription(`Welcome back ${message.author}! Your AFK status has been removed.`)
-                .setColor(0x57F287);
-
-            return message.channel.send({ embeds: [welcomeBack] }).then(msg => {
-                setTimeout(() => msg.delete(), 5000);
-            });
+            const welcomeBack = await message.reply("Welcome back! Your AFK status has been removed.");
+            setTimeout(() => welcomeBack.delete().catch(() => {}), 5000);
         }
 
         if (message.mentions.members.size > 0) {
-            message.mentions.members.forEach(member => {
-                if (afkUsers.has(member.id)) {
-                    const data = afkUsers.get(member.id);
-                    const afkEmbed = new EmbedBuilder()
-                        .setTitle(`${member.user.username} is AFK`)
-                        .setDescription(`**Reason:** ${data.reason}\n**Since:** <t:${Math.floor(data.time / 1000)}:R>`)
-                        .setColor(0xED4245);
-                    
-                    message.reply({ embeds: [afkEmbed] });
+            message.mentions.members.forEach((member) => {
+                const afkData = afkUsers.get(member.id);
+                if (afkData) {
+                    const embed = new EmbedBuilder()
+                        .setDescription(`**${member.displayName} is currently AFK**\n\n**Reason:** ${afkData.reason}\n**Since:** <t:${Math.floor(afkData.time / 1000)}:R>`)
+                        .setColor(0xFF434E);
+
+                    message.reply({ embeds: [embed] })
+                        .then(msg => {
+                            setTimeout(() => {
+                                msg.delete().catch(err => {});
+                            }, 15000);
+                        });
                 }
             });
         }
