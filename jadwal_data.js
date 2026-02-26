361cb9b9a21cee4992a0c640cdda5368a131437d
huanisalright <tworejib@gmail.com>
2026-02-26 15:57:38 +0700

    Update : Benerin Jadwal Kampus

--- jadwal_data.js
+++ jadwal_data.js
@@ -1,3 +1,16 @@
 module.exports = [
-    { hari: 1, jam: "1:00", matkul: "test" },
+    { hari: 1, jam: "07:00", matkul: "Ekonomi Rekayasa CC 07:00 - 08:50" },
+    { hari: 1, jam: "13:00", matkul: "Bangunan Air BC 13:00 - 14:50" },
+    { hari: 1, jam: "15:00", matkul: "Dinding Penahan Tanah CE 15:00 - 16:50 " },
+    { hari: 2, jam: "11:00", matkul: "Dinding Penahan Tanah CC 11:00 - 12:50" },
+    { hari: 2, jam: "13:00", matkul: "Rekayasa Pantai Dan Pelabuhan BB 13:00 - 15:50" },
+    { hari: 3, jam: "07:00", matkul: "Rekayasa Gedung AA 07:00 - 08:50" },
+    { hari: 3, jam: "13:00", matkul: "Rekayasa Pantai Dan Pelabuhan BC 13:00 - 14:50" },
+    { hari: 4, jam: "07:00", matkul: "Rekayasa Bandara CC 07:00 - 08.50" },
+    { hari: 4, jam: "09:00", matkul: "Mekanika Tanah II DD 09:00 - 10:50" },
+    { hari: 4, jam: "13:00", matkul: "Bangunan Air BB 13:00 - 15:50" },
+    { hari: 5, jam: "07:00", matkul: "Struktur Beton Bertulang Lanjut BB 07:00 - 09:50" },
+    { hari: 5, jam: "09:00", matkul: "Rekayasa Banda CE 09:00 - 11:00" },
+    { hari: 5, jam: "13:00", matkul: "Mekanika Tanah II BC 13:00 - 15:50" },
+    { hari: 6, jam: "09:00", matkul: "SBBL ASIS 09:00 - 13:00"}
 ];
