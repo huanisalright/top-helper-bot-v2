e8929da983df2a5e48c37e335d82119d47ca3b30
huanisalright <tworejib@gmail.com>
2026-02-27 19:32:30 +0700

    Final: Implemented modular system, English commands, and next class info feature

--- commands/admin/infojadwal.js
+++ commands/admin/infojadwal.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const { jadwalNext } = require('../../utils/embed.js');
 const jadwalData = require('../../jadwal_data.js');
 
